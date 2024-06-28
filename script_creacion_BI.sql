2ec9230fa826cdb580bb86d234e6d72b4acd2eae
tomaswatson <twatson@frba.utn.edu.ar>
2024-06-28 20:30:31 -0300

    cambio de nombres

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -43,7 +43,7 @@ create table PERSISTENTES.BI_tipoCaja
 	constraint PK_BI_tipoCaja primary key (tipo_caja)
 )
 
-create table PERSISTENTES.hechos_ventas
+create table PERSISTENTES.BI_hechos_ventas
 (
 	hechosVenta_id int identity,
 	hechosVenta_tiempo_id int not null,
@@ -58,23 +58,23 @@ create table PERSISTENTES.hechos_ventas
 	constraint PK_BI_hechosVentas primary key (hechosVenta_id)
 )
 
-ALTER TABLE PERSISTENTES.hechos_ventas
+ALTER TABLE PERSISTENTES.BI_hechos_ventas
 	add constraint FK_hechosVentas_tiempo
 	foreign key (hechosVenta_tiempo_id) references PERSISTENTES.BI_tiempo
 
-ALTER TABLE PERSISTENTES.hechos_ventas
+ALTER TABLE PERSISTENTES.BI_hechos_ventas
 	add constraint FK_hechosVentas_ubicacion
 	foreign key (hechosVenta_ubicacion_id) references PERSISTENTES.BI_ubicacion
 
-ALTER TABLE PERSISTENTES.hechos_ventas
+ALTER TABLE PERSISTENTES.BI_hechos_ventas
 	add constraint FK_hechosVentas_turno
 	foreign key (hechosVenta_turno_id) references PERSISTENTES.BI_turno
 
-ALTER TABLE PERSISTENTES.hechos_ventas
+ALTER TABLE PERSISTENTES.BI_hechos_ventas
 	add constraint FK_hechosVentas_tipoCaja
 	foreign key (hechosVenta_tipo_caja) references PERSISTENTES.BI_tipoCaja
 
-ALTER TABLE PERSISTENTES.hechos_ventas
+ALTER TABLE PERSISTENTES.BI_hechos_ventas
 	add constraint FK_hechosVentas_rangoEtario
 	foreign key (hechosVenta_rangoEtario_id) references PERSISTENTES.BI_rangoEtario
 
@@ -91,7 +91,7 @@ alter table PERSISTENTES.BI_sucursal
 	add constraint FK_BI_sucursal_ubicacion
 	foreign key (sucursal_ubicacion_id) references PERSISTENTES.BI_ubicacion
 
-create table PERSISTENTES.hechos_envios
+create table PERSISTENTES.BI_hechos_envios
 (
 	hechosEnvios_id int identity,
 	hechosEnvios_tiempo_id int not null,
@@ -104,19 +104,19 @@ create table PERSISTENTES.hechos_envios
 	constraint PK_BI_hechosEnvios primary key (hechosEnvios_id)
 )
 
-alter table PERSISTENTES.hechos_envios
+alter table PERSISTENTES.BI_hechos_envios
 	add constraint FK_hechosEnvios_tiempo
 	foreign key (hechosEnvios_tiempo_id) references PERSISTENTES.BI_tiempo
 
-alter table PERSISTENTES.hechos_envios
+alter table PERSISTENTES.BI_hechos_envios
 	add constraint FK_hechosEnvios_ubicacion
 	foreign key (hechosEnvios_ubicacion_id) references PERSISTENTES.BI_ubicacion
 
-alter table PERSISTENTES.hechos_envios
+alter table PERSISTENTES.BI_hechos_envios
 	add constraint FK_hechosEnvios_rangoEtario
 	foreign key (hechosEnvios_rangoEtario_id) references PERSISTENTES.BI_rangoEtario
 
-alter table PERSISTENTES.hechos_envios
+alter table PERSISTENTES.BI_hechos_envios
 	add constraint FK_hechosEnvios_sucursal
 	foreign key (hechosEnvios_sucursal_id) references PERSISTENTES.BI_sucursal
 
@@ -128,7 +128,7 @@ create table PERSISTENTES.BI_medioDePago
 	constraint PK_BI_medioDePago primary key (medio_de_pago)
 )
 
-create table PERSISTENTES.hechos_pagos
+create table PERSISTENTES.BI_hechos_pagos
 (
 	hechosPagos_id int identity,
 	hechosPagos_tiempo_id int not null,
@@ -143,19 +143,19 @@ create table PERSISTENTES.hechos_pagos
 	constraint PK_BI_hechosPagos primary key (hechosPagos_id)
 )
 
-alter table PERSISTENTES.hechos_pagos
+alter table PERSISTENTES.BI_hechos_pagos
 	add constraint FK_hechosPagos_tiempo
 	foreign key (hechosPagos_tiempo_id) references PERSISTENTES.BI_tiempo
 
-alter table PERSISTENTES.hechos_pagos
+alter table PERSISTENTES.BI_hechos_pagos
 	add constraint FK_hechosPagos_rangoEtario
 	foreign key (hechosPagos_rangoEtario_id) references PERSISTENTES.BI_rangoEtario
 
-alter table PERSISTENTES.hechos_pagos
+alter table PERSISTENTES.BI_hechos_pagos
 	add constraint FK_hechosPagos_sucursal
 	foreign key (hechosPagos_sucursal_id) references PERSISTENTES.BI_sucursal
 
-alter table PERSISTENTES.hechos_pagos
+alter table PERSISTENTES.BI_hechos_pagos
 	add constraint FK_hechosPagos_medioDePago
 	foreign key (hechosPagos_medioDePago) references PERSISTENTES.BI_medioDePago
 
@@ -222,8 +222,8 @@ insert into PERSISTENTES.BI_turno
 	(turno_descripcion)
 select 'otros'
 
---hechos_ventas
-insert into PERSISTENTES.hechos_ventas
+--BI_hechos_ventas
+insert into PERSISTENTES.BI_hechos_ventas
 	(hechosVenta_tiempo_id,hechosVenta_ubicacion_id,hechosVenta_turno_id,hechosVenta_tipo_caja,hechosVenta_rangoEtario_id,hechosVenta_importe,
 	hechosVenta_cantidad_unidades,hechosVenta_descuento)
 
@@ -263,7 +263,7 @@ from PERSISTENTES.Sucursal
 join PERSISTENTES.BI_ubicacion on ubicacion_localidad = sucursal_localidad_id
 
 --hechos envios
-insert into PERSISTENTES.hechos_envios
+insert into PERSISTENTES.BI_hechos_envios
 	(hechosEnvios_tiempo_id,hechosEnvios_ubicacion_id,hechosEnvios_rangoEtario_id,hechosEnvios_estado,hechosEnvios_sucursal_id,hechosEnvios_costo_envio)
 select 
 	(select tiempo_id from PERSISTENTES.BI_tiempo where tiempo_anio = year(envio_fecha_programada) and tiempo_mes = month(envio_fecha_programada)),
@@ -293,8 +293,8 @@ insert into PERSISTENTES.BI_medioDePago
 	(medio_de_pago,tipo_medio_de_pago)
 select medio_de_pago, medio_de_pago_tipo from PERSISTENTES.MedioDePago
 
---hechos_Pagos
-insert into PERSISTENTES.hechos_pagos
+--BI_hechos_Pagos
+insert into PERSISTENTES.BI_hechos_pagos
 	(hechosPagos_tiempo_id,hechosPagos_rangoEtario_id,hechosPagos_sucursal_id,hechosPagos_importe,hechosPagos_cantidad_cuotas,hechosPagos_cantidad_descontada,hechosPagos_medioDePago)
 select  
 	(select tiempo_id from PERSISTENTES.BI_tiempo where tiempo_anio = year(pago_fecha) and MONTH(pago_fecha) = tiempo_mes),
@@ -331,32 +331,32 @@ join PERSISTENTES.BI_medioDePago mp on mp.medio_de_pago = p.pago_medio_pago
 
 
 go
-/*1. Ticket Promedio mensual. Valor promedio de las ventas (en $) según la
-localidad, año y mes. Se calcula en función de la sumatoria del importe de las
+/*1. Ticket Promedio mensual. Valor promedio de las ventas (en $) segï¿½n la
+localidad, aï¿½o y mes. Se calcula en funciï¿½n de la sumatoria del importe de las
 ventas sobre el total de las mismas.*/
 
 create view PERSISTENTES.Ticket_Promedio_Mensual
 as
 select tiempo_anio,tiempo_mes,ubicacion_nombre, 
-sum(hechosVenta_importe) / (select sum(hechosVenta_importe) from PERSISTENTES.hechos_ventas) as Promedio_de_ventas
-from PERSISTENTES.hechos_ventas
+sum(hechosVenta_importe) / (select sum(hechosVenta_importe) from PERSISTENTES.BI_hechos_ventas) as Promedio_de_ventas
+from PERSISTENTES.BI_hechos_ventas
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosVenta_tiempo_id
 join PERSISTENTES.BI_ubicacion on hechosVenta_ubicacion_id = ubicacion_id
 group by tiempo_anio,tiempo_mes,ubicacion_nombre
 go
 --select * from PERSISTENTES.Ticket_Promedio_Mensual
 
-/*2. Cantidad unidades promedio. Cantidad promedio de artículos que se venden
-en función de los tickets según el turno para cada cuatrimestre de cada año. Se
-obtiene sumando la cantidad de artículos de todos los tickets correspondientes
-sobre la cantidad de tickets. Si un producto tiene más de una unidad en un ticket,
+/*2. Cantidad unidades promedio. Cantidad promedio de artï¿½culos que se venden
+en funciï¿½n de los tickets segï¿½n el turno para cada cuatrimestre de cada aï¿½o. Se
+obtiene sumando la cantidad de artï¿½culos de todos los tickets correspondientes
+sobre la cantidad de tickets. Si un producto tiene mï¿½s de una unidad en un ticket,
 para el indicador se consideran todas las unidades.*/
 go
 create view PERSISTENTES.Cantidad_Unidades_Promedio
 as
 select turno_descripcion, tiempo_cuatrimestre, tiempo_anio, 
-sum(hechosVenta_cantidad_unidades) / (select count(*) from PERSISTENTES.hechos_ventas) as Cantidad_unidades_promedio
-from PERSISTENTES.hechos_ventas
+sum(hechosVenta_cantidad_unidades) / (select count(*) from PERSISTENTES.BI_hechos_ventas) as Cantidad_unidades_promedio
+from PERSISTENTES.BI_hechos_ventas
 join PERSISTENTES.BI_turno on turno_id = hechosVenta_turno_id
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosVenta_tiempo_id
 group by turno_descripcion, tiempo_cuatrimestre, tiempo_anio
@@ -364,15 +364,15 @@ go
 --select * from PERSISTENTES.Cantidad_Unidades_Promedio
 go
 
-/*3. Porcentaje anual de ventas registradas por rango etario del empleado según el
+/*3. Porcentaje anual de ventas registradas por rango etario del empleado segï¿½n el
 tipo de caja para cada cuatrimestre. Se calcula tomando la cantidad de ventas
 correspondientes sobre el total de ventas anual.*/
 
 create view PERSISTENTES.Porcentaje_Anual_Ventas
 as
 select tiempo_anio, tiempo_cuatrimestre, rangoEtario_descripcion,tipo_caja,
-count(hechosVenta_id)*100.0 / (select count(hechosVenta_id) from PERSISTENTES.hechos_ventas) porcentaje_de_ventas
-from PERSISTENTES.hechos_ventas
+count(hechosVenta_id)*100.0 / (select count(hechosVenta_id) from PERSISTENTES.BI_hechos_ventas) porcentaje_de_ventas
+from PERSISTENTES.BI_hechos_ventas
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosVenta_tiempo_id
 join PERSISTENTES.BI_tipoCaja on tipo_caja = hechosVenta_tipo_caja
 join PERSISTENTES.BI_rangoEtario on rangoEtario_id = hechosVenta_rangoEtario_id
@@ -381,12 +381,12 @@ go
 
 --select * from PERSISTENTES.Porcentaje_Anual_Ventas
 
-/*4. Cantidad de ventas registradas por turno para cada localidad según el mes de
-cada año.*/
+/*4. Cantidad de ventas registradas por turno para cada localidad segï¿½n el mes de
+cada aï¿½o.*/
 
 create view PERSISTENTES.Cantidad_De_Ventas
 as
-select turno_descripcion, tiempo_mes, ubicacion_nombre, count(hechosVenta_id) cantidad_de_ventas from PERSISTENTES.hechos_ventas
+select turno_descripcion, tiempo_mes, ubicacion_nombre, count(hechosVenta_id) cantidad_de_ventas from PERSISTENTES.BI_hechos_ventas
 join PERSISTENTES.BI_turno on turno_id = hechosVenta_turno_id
 join PERSISTENTES.BI_ubicacion on hechosVenta_ubicacion_id = ubicacion_id
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosVenta_tiempo_id
@@ -395,37 +395,37 @@ go
 
 --select * from PERSISTENTES.Cantidad_De_Ventas
 
-/*5. Porcentaje de descuento aplicados en función del total de los tickets según el
-mes de cada año.*/
+/*5. Porcentaje de descuento aplicados en funciï¿½n del total de los tickets segï¿½n el
+mes de cada aï¿½o.*/
 
 create view PERSISTENTES.Porcentaje_Descuento_Aplicado
 as
-select tiempo_anio, tiempo_mes, sum(hechosVenta_descuento)*100.0 / (select sum(hechosVenta_descuento) from PERSISTENTES.hechos_ventas) Porcentaje_De_Descuento_Aplicado
-from PERSISTENTES.hechos_ventas
+select tiempo_anio, tiempo_mes, sum(hechosVenta_descuento)*100.0 / (select sum(hechosVenta_descuento) from PERSISTENTES.BI_hechos_ventas) Porcentaje_De_Descuento_Aplicado
+from PERSISTENTES.BI_hechos_ventas
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosVenta_tiempo_id
 group by tiempo_anio, tiempo_mes
 go
 
 --select * from PERSISTENTES.Porcentaje_Descuento_Aplicado
 
-/*6. Las tres categorías de productos con mayor descuento aplicado a partir de
-promociones para cada cuatrimestre de cada año.*/
+/*6. Las tres categorï¿½as de productos con mayor descuento aplicado a partir de
+promociones para cada cuatrimestre de cada aï¿½o.*/
 
 --TODO
 
-/*7. Porcentaje de cumplimiento de envíos en los tiempos programados por
-sucursal por año/mes (desvío)*/
+/*7. Porcentaje de cumplimiento de envï¿½os en los tiempos programados por
+sucursal por aï¿½o/mes (desvï¿½o)*/
 
 create view PERSISTENTES.Porcentaje_De_Cumplimiento_Envios
 as
 select tiempo_anio, tiempo_mes, sucursal_nombre, 
 	count(hechosEnvios_estado)*100.0 / 
-	(select count(*) from PERSISTENTES.hechos_envios
+	(select count(*) from PERSISTENTES.BI_hechos_envios
 	join PERSISTENTES.BI_tiempo on tiempo_id = hechosEnvios_tiempo_id
 	join PERSISTENTES.BI_sucursal on sucursal_id = hechosEnvios_sucursal_id
 	where tiempo_anio = bt.tiempo_anio and tiempo_mes = bt.tiempo_mes and bs.sucursal_nombre = sucursal_nombre
 	) porcentaje_de_cumplimiento 
-from PERSISTENTES.hechos_envios he
+from PERSISTENTES.BI_hechos_envios he
 join PERSISTENTES.BI_tiempo bt on tiempo_id = hechosEnvios_tiempo_id
 join PERSISTENTES.BI_sucursal bs on sucursal_id = hechosEnvios_sucursal_id
 where hechosEnvios_estado = 1
@@ -435,39 +435,39 @@ go
 
 --select * from PERSISTENTES.Porcentaje_De_Cumplimiento_Envios
 
-/*8. Cantidad de envíos por rango etario de clientes para cada cuatrimestre de
-cada año.*/
+/*8. Cantidad de envï¿½os por rango etario de clientes para cada cuatrimestre de
+cada aï¿½o.*/
 
 create view PERSISTENTES.Cantidad_De_Envios
 as
 select tiempo_anio, tiempo_cuatrimestre, rangoEtario_descripcion,
 	count(*) cantidad_de_envios
-from PERSISTENTES.hechos_envios
+from PERSISTENTES.BI_hechos_envios
 join PERSISTENTES.BI_rangoEtario on hechosEnvios_rangoEtario_id = rangoEtario_id
 join PERSISTENTES.BI_tiempo on hechosEnvios_tiempo_id = tiempo_id
 group by tiempo_anio,tiempo_cuatrimestre,rangoEtario_descripcion
 go
 --select * from PERSISTENTES.Cantidad_De_Envios
 
-/*9. Las 5 localidades (tomando la localidad del cliente) con mayor costo de envío.*/
+/*9. Las 5 localidades (tomando la localidad del cliente) con mayor costo de envï¿½o.*/
 
 create view PERSISTENTES.Localidades_con_mayor_costo_envios
 as
-select top 5 ubicacion_nombre from PERSISTENTES.hechos_envios
+select top 5 ubicacion_nombre from PERSISTENTES.BI_hechos_envios
 join PERSISTENTES.BI_ubicacion on ubicacion_id = hechosEnvios_ubicacion_id
 group by ubicacion_nombre
 order by sum(hechosEnvios_costo_envio) desc
 go
 --select * from PERSISTENTES.Localidades_con_mayor_costo_envios
 
-/*10. Las 3 sucursales con el mayor importe de pagos en cuotas, según el medio de
-pago, mes y año. Se calcula sumando los importes totales de todas las ventas en
+/*10. Las 3 sucursales con el mayor importe de pagos en cuotas, segï¿½n el medio de
+pago, mes y aï¿½o. Se calcula sumando los importes totales de todas las ventas en
 cuotas.*/
 
 create view PERSISTENTES.Sucursales_con_mayor_importe
 as
 select top 3 sucursal_nombre,tiempo_anio,tiempo_mes,hechosPagos_medioDePago
-from PERSISTENTES.hechos_pagos
+from PERSISTENTES.BI_hechos_pagos
 join PERSISTENTES.BI_sucursal on sucursal_id = hechosPagos_sucursal_id
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosPagos_tiempo_id
 join PERSISTENTES.BI_medioDePago on medio_de_pago = hechosPagos_medioDePago
@@ -478,12 +478,12 @@ go
 
 --select * from PERSISTENTES.Sucursales_con_mayor_importe
 
-/*11. Promedio de importe de la cuota en función del rango etareo del cliente.*/
+/*11. Promedio de importe de la cuota en funciï¿½n del rango etareo del cliente.*/
 
 create view PERSISTENTES.Promedio_Importe_Cuota
 as
 select rangoEtario_descripcion, sum(hechosPagos_importe/hechosPagos_cantidad_cuotas)/count(hechosPagos_importe) as Promedio_De_Importe_De_La_Cuota
-from PERSISTENTES.hechos_pagos
+from PERSISTENTES.BI_hechos_pagos
 join PERSISTENTES.BI_rangoEtario on hechosPagos_rangoEtario_id = rangoEtario_id
 where hechosPagos_cantidad_cuotas != 0
 group by rangoEtario_descripcion
@@ -491,17 +491,17 @@ go
 
 --select * from PERSISTENTES.Promedio_Importe_Cuota
 
-/*12. Porcentaje de descuento aplicado por cada medio de pago en función del valor
+/*12. Porcentaje de descuento aplicado por cada medio de pago en funciï¿½n del valor
 de total de pagos sin el descuento, por cuatrimestre. Es decir, total de descuentos
-sobre el total de pagos más el total de descuentos.*/
+sobre el total de pagos mï¿½s el total de descuentos.*/
 
 create view PERSISTENTES.Porcentaje_Descuento_Aplicado_Pagos
 as
 select medio_de_pago, tiempo_cuatrimestre,  
 	sum(hechosPagos_cantidad_descontada)*100.0/ sum(hechosPagos_cantidad_descontada+hechosPagos_importe) porcentaje_descuento_aplicado
 
---	(select sum(hechosPagos_cantidad_descontada) from PERSISTENTES.hechos_pagos) 
-from PERSISTENTES.hechos_pagos
+--	(select sum(hechosPagos_cantidad_descontada) from PERSISTENTES.BI_hechos_pagos) 
+from PERSISTENTES.BI_hechos_pagos
 join PERSISTENTES.BI_medioDePago on medio_de_pago = hechosPagos_medioDePago
 join PERSISTENTES.BI_tiempo on tiempo_id = hechosPagos_tiempo_id
 group by medio_de_pago, tiempo_cuatrimestre
